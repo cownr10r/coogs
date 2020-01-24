5c0f2aff6f861ae882677686b5344e2b0f384770
Mario Antonio Martinez <mario.a.martinez@live.com>
2020-01-23 19:51:13 -0600

    getting rid of potential bugs

--- R/mergeme.R
+++ R/mergeme.R
@@ -1,11 +1,11 @@
 #' merge two files
-#' @param b larger dataset
+#' @param b larger dataset, if there is a difference
 #' @param d smaller dataset
-#' @param e id field for original datasets in common
+#' @param e id field for original datasets in common. Should be surrounded by quotes
 #' @return a merged set with inner, outer, and all joins.
 mergeme <- function(b,d,e){ #b is larger dataset
-  this <- merge(b,d, all.y = T, by = "e")
-  that <- merge(b,d, all.x = T, by = "e")
+  this <- merge(b,d, all.y = T, by = paste0(a,e))
+  that <- merge(b,d, all.x = T, by = paste0(a,e))
   that$id <- 1:length(that[,1]) #larger
   this$id <- 1:length(this[,1])
   other <- merge(that, this, all.x = T, by = "id")
