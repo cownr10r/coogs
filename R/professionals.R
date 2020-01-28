416b6fccf9088c67059aebb1356c69dddab5f46b
Mario Antonio Martinez <mario.a.martinez@live.com>
2020-01-28 17:45:37 -0600

    updating description

--- R/professionals.R
+++ R/professionals.R
@@ -1,10 +1,10 @@
 #' Identify professional certs
-#' @param a = filename
+#' @param a file name
 #' @return file with only professional certifications
 #' @export
 
 
-professionals <- function(a = filename){
+professionals <- function(a ){
   #source: https://stackoverflow.com/questions/22249702/delete-rows-containing-specific-strings-in-r
   d <- a[grepl("Principal|Superintendent|School Counselor|Reading Specialist", a$Test.Name),]
   return(d)
