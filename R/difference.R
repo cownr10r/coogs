332668b43b5d70ce54b2b919d69f6693205ecd82
Mario Antonio Martinez <mario.a.martinez@live.com>
2020-02-01 12:29:00 -0600

    corrected bug

--- R/difference.R
+++ R/difference.R
@@ -1,22 +1,18 @@
-#' examine the difference between newer datasets
-#' @param a smaller data frame
-#' @param b larger data frame
-#' @param d column number for first data frame ID
-#' @param p column number for second data frame ID
-#' @param m column number for first index number
-#' @param n column number for second index number
-#' @return a data set of rows that remain after comparison
+#' Examine the difference between newer datasets
+#' @param a smaller datafile
+#' @param b larger datafile
+#' @param d column number for ID 1
+#' @param e column number for ID 2
+#' @param f column number for index 1
+#' @param g columnn number for index 2
+#' @return the columns that remain
 #' @export
 
-
-
-difference <- function(a = smaller_datafile, b = larger_datafile, d = column_number_for_id_1, p = column_for_id_2, m = column_for_index1, n = column_number_for_index_2){
-  f <- a[order(a[,d]),]
-  g <- b[order(b[,p]),]
-  f$num <- 1:length(f[,1])
-  g$num <- 1:length(g[,1])
-  e <- setdiff(g[,m], f[,n])
-  h <- paste0(e)
-  l <- b[c(h),]
-  return(l)
+difference <- function(a,b,d,e,f,g){
+  h <- a[order(a[,d]),]
+  j <- b[order(b[,e]),]
+  k <- setdiff(j[,g], h[,f])
+  l <- paste0(k)
+  m <- b[c(l),]
+  return(m)
 }
