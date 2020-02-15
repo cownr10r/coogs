17eab0afa747bbc91b5446a39eac162520c4d240
Mario Antonio Martinez <mario.a.martinez@live.com>
2020-02-15 11:08:03 -0600

    included new developments in difference

--- R/difference.R
+++ R/difference.R
@@ -1,7 +1,7 @@
 #' Examine the difference between newer datasets
 #' @param a smaller datafile
 #' @param b larger datafile
-#' @return the differences that remain
+#' @return the row differences that remain
 #' @export
 
 difference <- function(a,b){
@@ -10,8 +10,8 @@ difference <- function(a,b){
   a$id <- 1:nrow(a)
   b$id <- 1:nrow(b)
   d <- setdiff(b$id,a$id)
-  e <- paste(d, collapse="|")
-  f <- two[grepl(e, two$id),]
+  ifelse(length(d) == 1, e <- paste0("", d), e <- paste0("", d, collapse = "|"))
+  f <- b[grepl(e, b$id),]
   g <- that[rev(order(f$Admin.Date)),]
   return(g)
 }
